21bb2b33e33beca6196052684faea0bca582544e
limnoliver <soliver@usgs.gov>
2022-01-04 15:35:56 -0600

    added data descriptions, code files,

--- src/file_utils.R
+++ src/file_utils.R
@@ -1,9 +1,13 @@
 fetch_network <- function(rds_in) {
-  site_ids <- c(get_seg_ids(table = 1), get_seg_ids(table = 3))
+  site_ids_s1 <- get_seg_ids(table = 1)
+  site_ids_s2 <- get_seg_ids(table = 3)
+  site_ids <- c(site_ids_s1, site_ids_s2)
   sf_object <- readRDS(rds_in)
+
   sf_filtered <- sf_object$edges %>%
     select(seg_id_nat) %>%
     filter(seg_id_nat %in% site_ids & !is.na(seg_id_nat)) %>%
+    mutate(region = ifelse(seg_id_nat %in% site_ids_s2, 'S2', 'S1')) %>%
     sf::st_transform(crs = 4326) %>%
     distinct()
   return(sf_filtered)
